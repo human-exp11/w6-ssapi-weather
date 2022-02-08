db940128e0f791c39bddecfc4ff3629c505ba635
Luca Urbanowicz <human.exp11@gmail.com>
2022-02-07 22:45:16 -0700

    addended JS and updated README

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # w6-ssapi-weather
-weather dashboard
+Weather Dashboard
+
+***Weather Dashboard***
+
+I created a Weather Dashboard using 
+
+## Acceptance Criteria
+
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+
+## Completed Steps:
+
+* Created my own API Key using the Open Weather API webpage
+* Utilized two different API's to create both current and future weather conditions specific to the city searched
+* Utilized both BootStrap and JQuery to create my HTML and styling
+* Traversed the DOM to refer to specific code 
+* Utilized a forloop to call upon data related to the 5-Day Forecast
+* Created a function to call previous city data from local storage
+* Created README file with link & screenshot
+
+[Live-site] (https://human-exp11.github.io/w6-ssapi-weather/)
+
+![Screenshot](./git-images/wds.png.)
--- script.js
+++ script.js
@@ -12,8 +12,26 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-
 var APIkey = "d695cd030163d3bbeb3c1729339cf830";
+const cityInput = $("#city-submit")
+const cityEl = $("#city-name")
+const prevCityList = $("#prev-cities-container")
+let prevCities = [ ];
+
+function init() {
+    // Checking to see if the key already exists, if not, set an empty array converted to a string to localstorage
+    if (localStorage.getItem("prevCities") === null) {
+        localStorage.setItem("prevCities", JSON.stringify(prevCities));
+    };
+
+    // Immediately parse that array so we can push to it as cities are searched
+    prevCities = JSON.parse(localStorage.getItem("prevCities"));
+
+    // If there are any cities that have been pulled down from localStorage, run the function to append a button for each
+    updatePrevUI(prevCities);
+}
+
+
 
 function searchHandler(e) {
     e.preventDefault();
@@ -25,13 +43,22 @@ function searchHandler(e) {
         console.log (data);
        var  lat = data.coord.lat
        var  lon = data.coord.lon
-        var oneCallAPIURL= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid="+APIkey;
+        var oneCallAPIURL= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +  "&units=imperial&appid="+APIkey;
         fetch(oneCallAPIURL).then(function(response){
             return response.json()
         }) .then(function(data){
             console.log (data);
             displayCurrent(data);
+            fiveDay(data);
         })
+        if (prevCities.includes(userCity) === false) {
+            prevCities.push(userCity);
+        }
+
+        // Uodating the recentCities array in localStorage
+        localStorage.setItem("prevCities", JSON.stringify(prevCities));
+
+        updatePrevUI(prevCities);
     })
 }
 
@@ -39,71 +66,74 @@ function displayCurrent(weather) {
     var currentWeatherEl = $("#currentWeather")
     var cardText = currentWeatherEl.children("div.card-body")
     cardText.children("h5").text(weather.timezone)
-    cardText.children("h6").text(weather.current.dt)
+    var formatTime = new Date(weather.current.dt * 1000)
+    cardText.children("h6").text(formatTime)
     var iconUrl = "http://openweathermap.org/img/wn/" + weather.current.weather[0].icon + "@2x.png"
     cardText.children("img").attr("src", iconUrl)
-    cardText.children("#temperature").text(weather.current.temp)
-    cardText.children("#humidity").text(weather.current.humidity)
-    cardText.children("#windSpeed").text(weather.current.wind_speed)
-    cardText.children("#uvIndex").text(weather.current.uvi)
+    cardText.children("#temperature").text(`Current Temp: ${weather.current.temp}F`)
+    cardText.children("#humidity").text(`Current Humidity: ${weather.current.humidity}`)
+    cardText.children("#windSpeed").text(`Current Wind Speed: ${weather.current.wind_speed} mph`)
+    cardText.children("#uvIndex").text(`Current UV Index: ${weather.current.uvi}`)
+}
+
+function fiveDay(weather){
+    var forecastElement = $("#five")
+    for (let index = 0; index < 5; index++) {
+       var forecastDiv =  $("<div></div>")
+        // console.log(weather)
+        var dailyStuff = weather.daily[index]
+        console.log(dailyStuff)
+        
+        var dailyTime = new Date(dailyStuff.dt * 1000).classList.$("date")
+        forecastDiv.text(dailyTime)
+
+        var forecastImg =  $("<img></img>")
+        var fiveDayUrl = "http://openweathermap.org/img/wn/" + weather.current.weather[0].icon + "@2x.png"
+        forecastImg.attr("src", fiveDayUrl)
+
+        var tempP = $("<p></p>")
+        var dailyTemp = dailyStuff.temp.day
+        tempP.text(dailyTemp)
+
+        var windP = $("<p></p>")
+        var dailyWind = dailyStuff.wind_speed
+        windP.text(dailyWind)
+
+        var humidP = $("<p></p>")
+        var dailyHumid = dailyStuff.temp.humidity
+        humidP.text(dailyHumid)
+
+        
+        forecastElement.append(windP)
+        forecastElement.append(humidP)
+        forecastElement.append(tempP)
+        forecastDiv.append(forecastImg)
+        forecastElement.append(forecastDiv)
+
+    }
+
 }
 
 
+function updatePrevUI(array) {
+    prevCityList.empty();
 
-//------ 5-Day Forecast
-
-fetchFiveDay = function () {
-    var fiveDayEl = $("#fiveDay")
-    var perfectAPIURL = "https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=" + APIkey;
-
-    fetch(perfectAPIURL).then(function (response) {
-        if (200 !== response.status) {
-            console.log(
-                "Uh Oh. Status Code: " + response.status
-            );
-            return response.json();
-        }
-
-        fiveDayEl[0].classList.add("loaded")
-
-        response.json().then(function (data) {
-            var fday = "";
-            data.daily.forEach((value, index) => {
-                if (index > 0) {
-                    var dayname = new Date(value.dt * 1000).toLocaleDateString("en", {
-                        weekday: "long",
-                    });
-                    var icon = value.weather[0].icon;
-                    var temp = value.temp.day.toFixed(0);
-                    fday = `<div class="forecast-day">
-						<p>${dayname}</p>
-						<p><span class="ico-${icon}" title="${icon}"></span></p>
-						<div class="forecast-day--temp">${temp}<sup>°C</sup></div>
-					</div>`;
-                    fiveDayEl[0].insertAdjacentHTML('beforeend', fday);
-
-                    return response.json();
-                    // .catch(function (err) {
-                    //     console.log("Fetch Error :-S", err);
-                }
-
-            });
-        });
+    for (let i = 0; i < array.length; i++) {
+        let prevCityItem = document.createElement("li");
+        prevCityIten.textContent = array[i];
+        prevCityList.append(prevCityItem);
+    }
+}
 
 
-    });
+document.addEventListener('click', prevCityListener);
 
-
-};
-
-
-
-//function fiveDay(weather){
-    //createEL and set vale
-    //append to section
-
-   // fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + weatherData.name + "&appid=ce39e7239416ad754359ca762d28521a&units=imperial")
-//}
-
+function prevCityListener(event) {
+    var element = event.target;
+    if (element.tagName ==  element.classList.contains("btn-info")) {
+        userCity.val(element.textContent);
+        searchHandler();
+    }
+}
 
 $("button").on("click", searchHandler)
